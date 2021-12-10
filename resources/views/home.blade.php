baca34435e8a7c7e4bc93137b5d72301cf8aacd6
Erwin Gavino <erwingavino48@gmail.com>
2021-12-10 07:45:24 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,117 @@
                 </div>
             </div>
         </div> -->
+        <div class="col-md-3" style="max-height:500px; overflow-y:auto;">
+          <div class="card" style="width: 100%;">
+            <div class="card-header">
+              Barber Shops List
+            </div>
+            <ul class="list-group list-group-flush" id="shops-list">
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-9" style="width: 500px; height: 500px">
+          <script type="text/javascript">
+          let map;
+          var shops = [];
+          const philippines = { lat: 15.5000569, lng: 120.9109837 };
 
-        <div class="col-md-12" style="width: 500px; height: 500px">
+          async function getShops() {
+            let response = await fetch ('http://localhost:8000/api/shops');
+            let data = await response.json();
+            return data;
+          };
+
+          function listShops(data) {
+            // console.log(data);
+            Object.entries(data.shops).forEach(([key, value]) => {
+              document.getElementById("shops-list").innerHTML += '<li class="list-group-item">' + value.name +'</li>'
+              shops.push({
+                'title':  value.name,
+                'position': {lat: value.lat, lng: value.lng},
+              });
+              console.log(shops);
+            });
+          }
+
+
+
+            async function initMap() {
+              await getShops()
+              .then(
+                data => listShops(data)
+              );
+
+              map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: 15.5000569, lng: 120.9109837 },
+                zoom: 8,
+              });
+
+              var infowindow = new google.maps.InfoWindow();
+              for (var i = 0; i < shops.length; i++) {
+                var shop = shops[i];
+                var latlng = new google.maps.LatLng(shop.position.lat, shop.position.lng);
+
+                // var infowindow = new google.maps.InfoWindow({
+                //   content: contentString,
+                // });
+
+                var marker = new google.maps.Marker({
+                  position: latlng,
+                  map: map,
+                  title: shop.title
+                });
+
+                var contentString =
+                  '<div id="content">' +
+                  '<div id="siteNotice">' +
+                  "</div>" +
+                  '<h3 id="firstHeading" class="firstHeading">'+ shop.title +'</h3>' +
+                  '<div id="bodyContent">' +
+                  "<p><b>"+ shop.title +"</b>" +
+                  "</div>" +
+                  "</div>";
+
+                attachInfoWindow(marker, contentString);
+                function attachInfoWindow(marker, info) {
+                  const infowindow = new google.maps.InfoWindow({
+                    content: info,
+                  });
+
+                  marker.addListener("click", () => {
+                    infowindow.open(marker.get("map"), marker);
+                  });
+                }
+
+                // google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                //   return function() {
+                //     // close all the other infowindows that opened on load
+                //     google.maps.event.trigger(map, 'click')
+                //     infowindow.setContent(contentString);
+                //     infowindow.open(map, marker);
+                //   }
+                // })(marker, i));
+
+                // marker.addListener("click", () => {
+                //   infowindow.open({
+                //     anchor: marker,
+                //     map,
+                //     shouldFocus: false,
+                //   });
+                // });
+                console.log(marker);
+              }
+              // marker.setMap(map);
+
+
+
+              // google.maps.event.addListener(marker, 'dragend', function(event){
+              //   // When marker is dragged, do this
+              //   document.getElementById("lat").value = event.latLng.lat();
+              //   document.getElementById("lng").value = event.latLng.lng();
+              // });
+            }
+          </script>
           <div id="map" style="width:100%;height:100%"></div>
           <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
           <script async src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_KEY') }}&callback=initMap"></script>
