14f9f7645a39b48c51974f2deae27a5e526706d2
Erwin Gavino <erwingavino48@gmail.com>
2022-03-30 03:22:20 -0400

    Revert "Update User.php"
    This reverts commit a3ff0be969ae85856b54dee1c902653afa879439.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Contracts\Auth\CanResetPassword;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, CanResetPassword;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
