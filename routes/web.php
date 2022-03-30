92a799638765155e94dcd59b79e7171f6c3ceaa1
Erwin Gavino <erwingavino48@gmail.com>
2022-03-30 03:02:18 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,10 @@ Route::get('/', function () {
 
 // Laravel Auth Routes
 Auth::routes(); //Handles functions for Laravel's Authentication
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->middleware('guest')->name('password.request');
+
 // Shop Owner Registration Route
 Route::get('/register/shopowner', [App\Http\Controllers\ShopOwnerController::class, 'showRegistrationForm'])->name('register.shopowner'); //Shows Registration  Page for Shopowners
 Route::post('/register/shopowner/{pending_request?}', [App\Http\Controllers\ShopOwnerController::class, 'register'])->name('register.shopowner'); //Handles functions for Shopowner Registration Page
