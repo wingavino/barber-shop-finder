79a2bd2d4ea9523cf181fe4a110018138dc19673
Erwin Gavino <erwingavino48@gmail.com>
2022-04-24 07:59:30 -0400

    Update shop-queue.blade.php

--- resources/views/shopowner/shop-queue.blade.php
+++ resources/views/shopowner/shop-queue.blade.php
@@ -21,6 +21,9 @@
                           <h3>
                             @isset($shop->queue->current_ticket)
                               <h3>{{ $shop->queue->current_ticket }}</h3>
+                              @if(App\Models\Ticket::where('queue_id', $shop->queue->id)->where('ticket_number', $shop->queue->current_ticket)->first()->user)
+                              <h3>Name: {{ App\Models\Ticket::where('queue_id', $shop->queue->id)->where('ticket_number', $shop->queue->current_ticket)->first()->user->name }}</h3>
+                              @endif
                             @else
                               <button class="btn btn-danger disabled" type="button" name="button">None</button>
                             @endisset
@@ -75,14 +78,19 @@
                                       <button class="btn btn-danger disabled" type="button" name="button">None</button>
                                     </div>
                                     @else
-                                    @foreach($shop->queue->ticket->where('on_hold', true) as $on_hold_ticket)
-                                    <div class="col">
-                                      <form method="POST" action="{{ route('shopowner.shop.queue.next', ['id' => $on_hold_ticket->id]) }}">
-                                        @csrf
-                                        <button class="btn btn-primary" type="submit" name="button">{{ $on_hold_ticket->ticket_number }}</button>
-                                      </form>
-                                    </div>
-                                    @endforeach
+                                      @foreach($shop->queue->ticket->where('on_hold', true) as $on_hold_ticket)
+                                      <div class="col">
+                                        <form method="POST" action="{{ route('shopowner.shop.queue.next', ['id' => $on_hold_ticket->id]) }}">
+                                          @csrf
+                                          <button class="btn btn-primary" type="submit" name="button">
+                                            {{ $on_hold_ticket->ticket_number }}
+                                            @if(App\Models\Ticket::where('queue_id', $shop->queue->id)->where('ticket_number', $on_hold_ticket->ticket_number)->first()->user)
+                                            <h3>Name: {{ App\Models\Ticket::where('queue_id', $shop->queue->id)->where('ticket_number', $on_hold_ticket->ticket_number)->first()->user->name }}</h3>
+                                            @endif
+                                          </button>
+                                        </form>
+                                      </div>
+                                      @endforeach
                                     @endempty
                                   </div>
                                 </div>
