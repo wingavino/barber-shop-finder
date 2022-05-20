e7e380260e119016e282ff9a03abc5221b7e31d5
Erwin Gavino <erwingavino48@gmail.com>
2022-05-20 02:10:39 -0400

    Update home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.app')
 
 @section('custom-scripts')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 <!-- Script to initialize map and Retrieve list of shops -->
-<script type="text/javascript" src="{{ asset('js/maps/index.js') }}"></script>
+<script type="text/javascript" async>
+  var app_url = "{{env('APP_URL')}}";
+</script>
+
+<script defer type="text/javascript" src="{{ asset('js/maps/index.js') }}"></script>
+
+<script defer type="text/javascript" src="{{ asset('js/maps/search.js') }}"></script>
 
 <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
-<script async src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_KEY') }}&callback=initMap"></script>
+<script defer src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_KEY') }}&callback=initMap"></script>
+
+<script type="text/javascript" src="{{ asset('js/requestAlert.js') }}"></script>
 @endsection
 
 @section('content')
@@ -13,14 +23,22 @@
     <div class="row justify-content-center">
         <div class="col-md-3" style="max-height:500px; overflow-y:auto;">
           <div class="card" style="width: 100%;">
-            <div class="card-header">
-              Barber Shops List
+            <div class="card-header text-center">
+              <a class="text-dark" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                <h5 class="card-title">Barber Shops List</h5>
+              </a>
+              <form>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="search" placeholder="Search">
+                </div>
+              </form>
             </div>
-            <ul class="list-group list-group-flush" id="shops-list">
-            </ul>
+            <div class="collapse show" id="collapseExample">
+              <ul class="list-group list-group-flush" id="shops-list">
+              </ul>
+            </div>
           </div>
         </div>
-
         <div class="col-md-9" style="width: 500px; height: 500px">
           <div id="map" style="width:100%;height:100%"></div>
         </div>
