9dc273c46c67de234e6e7e1c644da3ca7cba8397
Erwin Gavino <erwingavino48@gmail.com>
2022-05-24 19:35:11 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -181,7 +181,7 @@ Route::middleware('isAdmin')->group(function(){
   Route::get('/admin/shops/delete/{id}', [App\Http\Controllers\ShopController::class, 'showDeleteShop'])->name('admin.shops.delete'); //Shows Delete Shop Page
   Route::post('/admin/shops/delete/{id}', [App\Http\Controllers\ShopController::class, 'deleteShop'])->name('admin.shops.delete'); //Handles functions for Deleting a Shop
   Route::post('/admin/shop/reviews/delete/{id}', [App\Http\Controllers\ReviewController::class, 'deleteReview'])->name('admin.shops.reviews.delete'); //Handles functions for deleting a Shop Review
-  Route::get('/admin/shop/settings', [App\Http\Controllers\ShopController::class, 'showShopSettingsAsOwner'])->name('admin.shop.settings'); //Shows Shop's Settings Page for Shopowner
+  Route::get('/admin/shop/settings', [App\Http\Controllers\ShopController::class, 'showShopSettings'])->name('admin.shop.settings'); //Shows Shop's Settings Page for Shopowner
   Route::post('/admin/shops/approve/{id}', [App\Http\Controllers\ShopController::class, 'approveShop'])->name('admin.shops.approve'); //Handles functions for Approving a New Shop
 });
 
