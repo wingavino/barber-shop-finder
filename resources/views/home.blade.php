d4c4bff7434c53e7bc9a1211aa31d4260643f09c
Erwin Gavino <erwingavino48@gmail.com>
2022-05-24 10:41:26 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,66 +20,73 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-12">
-        <div class="card" style="width: 100%;">
-          <div class="card-header">
-            <div class="row">
-              <div class="col-12">
-                <form>
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="search" placeholder="Search">
+  <div class="row justify-content-center">
+    <div class="col-12 col-lg-4" style="max-height:500px; overflow-y:auto;">
+      <div class="card" style="width: 100%;">
+        <div class="card-header text-center">
+          <div class="row">
+            <div class="col-12">
+              <form>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="search" placeholder="Search">
+                </div>
+              </form>
+            </div>
+          </div>
+          <a class="text-dark" data-toggle="collapse" href="#collapseFilter" role="button" aria-expanded="false" aria-controls="collapseFilter">
+            <h5 class="card-title">Filter:</h5>
+          </a>
+        </div>
+        <div class="collapse show" id="collapseFilter">
+          <ul class="list-group list-group-flush" id="filter">
+            <li class="list-group-item">
+              By Shop Type:
+              <div class="row">
+                <div class="col-12">
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="" value="salon" >
+                    <label class="form-check-label" for="inlineCheckbox1">Salon</label>
                   </div>
-                </form>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <label>Filter By Shop Type: </label>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="checkbox" id="" value="salon" checked>
-                  <label class="form-check-label" for="inlineCheckbox1">Salon</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="checkbox" id="" value="barber" checked>
-                  <label class="form-check-label" for="inlineCheckbox1">Barber</label>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="" value="barber" >
+                    <label class="form-check-label" for="inlineCheckbox1">Barber</label>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <label>Filter By Service: </label>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="checkbox" id="" value="haircut" checked>
-                  <label class="form-check-label" for="inlineCheckbox1">Haircut</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="checkbox" id="" value="hairdye">
-                  <label class="form-check-label" for="inlineCheckbox1">Hair Dye</label>
+            </li>
+            <li class="list-group-item">
+              By Service:
+              <div class="row">
+                <div class="col-12">
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="" value="haircut">
+                    <label class="form-check-label" for="inlineCheckbox1">Haircut</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="" value="hairdye">
+                    <label class="form-check-label" for="inlineCheckbox1">Hair Dye</label>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="card" style="width: 100%;">
+        <div class="card-header text-center">
+          <a class="text-dark" data-toggle="collapse" href="#collapseShopList" role="button" aria-expanded="false" aria-controls="collapseShopList">
+            <h5 class="card-title">Barber Shops List</h5>
+          </a>
+        </div>
+        <div class="collapse show" id="collapseShopList">
+          <ul class="list-group list-group-flush" id="shops-list">
+          </ul>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12 order-first col-sm-12 order-sm-first col-md-3 order-md-first" style="max-height:500px; overflow-y:auto;">
-        <div class="card" style="width: 100%;">
-          <div class="card-header text-center">
-            <a class="text-dark" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-              <h5 class="card-title">Barber Shops List</h5>
-            </a>
-          </div>
-          <div class="collapse show" id="collapseExample">
-            <ul class="list-group list-group-flush" id="shops-list">
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 order-last col-sm-12 order-sm-last col-md-9 order-md-last" style="width: 500px; height: 500px">
-        <div id="map" style="width:100%;height:100%"></div>
-      </div>
+    <div class="col-12 col-lg-8 order-last" style="width: 500px; height: 500px">
+      <div id="map" style="width:100%;height:100%"></div>
     </div>
+  </div>
 </div>
 @endsection
