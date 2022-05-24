41d4c3256e3d0ac1cfc9695be2f4e840aa7c25b3
Erwin Gavino <erwingavino48@gmail.com>
2022-05-24 19:33:59 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -171,7 +171,7 @@ Route::middleware('isAdmin')->group(function(){
   Route::get('/admin/shop/images/upload', [App\Http\Controllers\ImageController::class, 'showUploadImage'])->name('admin.shop.images.upload'); //Shows Shop's Upload Image Page for Shopowner
   Route::post('/admin/shop/images/upload', [App\Http\Controllers\ImageController::class, 'uploadImage'])->name('admin.shop.images.upload'); //Handles functions for Uploading Shop Image
   Route::post('/admin/shop/images/logo/upload', [App\Http\Controllers\ImageController::class, 'uploadLogo'])->name('admin.shop.images.logo.upload'); //Handles functions for uploading Shop Logo Image
-  Route::get('/admin/reviews', [App\Http\Controllers\ShopController::class, 'showShopReviewsAsOwner'])->name('admin.shop.reviews'); //Shows Shop's Reviews Page for Shopowner
+  Route::get('/admin/shop/{id}/reviews', [App\Http\Controllers\ShopController::class, 'showShopReviews'])->name('admin.shop.reviews'); //Shows Shop's Reviews Page for Shopowner
   Route::get('/admin/shop/services/add', [App\Http\Controllers\ShopController::class, 'showAddShopServices'])->name('admin.shop.services.add'); //Shows Add Service Page for Shop
   Route::get('/admin/shop/{id}/services', [App\Http\Controllers\ShopController::class, 'showShopServices'])->name('admin.shop.services'); //Shows Shop's Services Page for Shopowner
   Route::post('/admin/shop/services/add', [App\Http\Controllers\ShopController::class, 'AddShopServices'])->name('admin.shop.services.add'); //Handles functions for Adding a Shop Service
