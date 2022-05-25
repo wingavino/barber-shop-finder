0b39a5305cf1e38272be3bb7ff251c5502979a22
Erwin Gavino <erwingavino48@gmail.com>
2022-05-24 23:58:28 -0400

    Update deleteModal.js

--- public/js/deleteModal.js
+++ public/js/deleteModal.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var button = $(event.relatedTarget); // Button that triggered the modal
     var formAction = button.data('form-action');
     var id = button.data('id'); // Extract info from data-* attributes
-    var service_id = button.data('service_id');
+    var service_id = button.data('service-id');
     var name = button.data('name');
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
