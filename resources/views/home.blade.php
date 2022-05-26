c52a8d8b9d811669692e52b264b9903dd8edb918
Erwin Gavino <erwingavino48@gmail.com>
2022-05-25 20:02:16 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,13 +63,17 @@
               <div class="row">
                 <div class="col-12">
                   <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="" value="all" checked>
+                    <label class="form-check-label" for="inlineCheckbox1">All</label>
+                  </div>
+                  <!-- <div class="form-check form-check-inline">
                     <input class="form-check-input" type="checkbox" id="" value="haircut">
                     <label class="form-check-label" for="inlineCheckbox1">Haircut</label>
                   </div>
                   <div class="form-check form-check-inline">
                     <input class="form-check-input" type="checkbox" id="" value="hairdye">
                     <label class="form-check-label" for="inlineCheckbox1">Hair Dye</label>
-                  </div>
+                  </div> -->
                 </div>
               </div>
             </li>
