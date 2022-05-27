3cc8958f7b5f8da9d15acac7966420cd4ec42d7d
Erwin Gavino <erwingavino48@gmail.com>
2022-05-27 00:04:28 -0400

    Update search.js

--- public/js/maps/search.js
+++ public/js/maps/search.js
@@ -1,8 +1,24 @@
-$(document).ready(function(){  
+$(document).ready(function(){
   $("#search").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#shops-list a").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
+
+  $('#collapseFilter').on('show.bs.collapse', function () {
+    $('#collapseFilterText').text("Filter -");
+  })
+
+  $('#collapseFilter').on('hide.bs.collapse', function () {
+    $('#collapseFilterText').text("Filter +");
+  })
+
+  $('#collapseShopList').on('show.bs.collapse', function () {
+    $('#collapseShopListText').text("Shops List -");
+  })
+
+  $('#collapseShopList').on('hide.bs.collapse', function () {
+    $('#collapseShopListText').text("Shops List +");
+  })
 });
