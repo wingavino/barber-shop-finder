9271be239aa7c8a4d530f1c3cbeb4210347bfabc
Erwin Gavino <erwingavino48@gmail.com>
2022-05-28 20:03:12 -0400

    Update map.js

--- public/js/maps/map.js
+++ public/js/maps/map.js
@@ -250,9 +250,11 @@ $(document).ready(function(){
           });
 
           if (getDistance(device, marker) > $("#max_distance").val() && radiusCircle.radius > 0) {
+            console.log('Removed' + shop.name + ' not within max distance of ' + $("#max_distance").val() + ' km. Radius: ' + radiusCircle.radius);
             marker.setMap(null);
             return false;
           }
+          console.log('Added ' + shop.name + ' within max distance of ' + $("#max_distance").val() + ' km. Radius: ' + radiusCircle.radius);
 
           markers.push(marker);
 
