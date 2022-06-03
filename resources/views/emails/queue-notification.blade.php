d0fb85e8e5aeefec35189e755ca422f011498828
Erwin Gavino <erwingavino48@gmail.com>
2022-06-03 15:13:11 -0400

    Update Email Notifications

--- resources/views/emails/queue-notification.blade.php
+++ resources/views/emails/queue-notification.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Update
-
-This is a friendly reminder that you are next in line to be serviced. Please try to arrive at the shop as soon as possible to avoid being put On Hold.
+Hi,
+We're just here to inform that you are next in line to be serviced. Please try to arrive at the shop as soon as possible to avoid being put On Hold.
 
 @component('mail::button', ['url' => 'saber.hair'])
 Click here to view the site
