ce407dcb008bc41327cc9e373dff96463c80e768
Erwin Gavino <erwingavino48@gmail.com>
2022-06-11 11:03:10 -0400

    Update QueueController.php

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -21,12 +21,12 @@ class QueueController extends Controller
     }
 
     $on_hold_tickets = Ticket::where('queue_id', $queue_id)->where('on_hold', true)->get();
-
+    $queue_length = Ticket::where('queue_id', $queue_id)->where('on_hold', false)->get()->count();
     if($request->ajax()){
       if ((Auth::user()->type == 'shopowner' && Auth::user()->shop->id == $queue->shop->id) OR Employee::where('user_id', Auth::user()->id)->where('shop_id', $queue->shop->id)->first()) {
         return response()->json(array('queue'=>$queue, 'current_ticket'=>$current_ticket, 'on_hold_tickets' => $on_hold_tickets, 'user'=>$user));
       }else {
-        return response()->json(array('queue'=>$queue));
+        return response()->json(array('queue'=>$queue, 'queue_length' => $queue_length));
       }
     }
 
