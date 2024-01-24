4466f61a85a5d3d4cf552c569dd409db43585ce0
Erwin Gavino <erwingavino48@gmail.com>
2024-01-24 02:46:03 -0500

    Update shop-nav-tabs.blade.php

--- resources/views/shop-nav-tabs.blade.php
+++ resources/views/shop-nav-tabs.blade.php
@@ -48,42 +48,6 @@
           {{ __('Reviews') }}
         </a>
       </li>
-    @elseif(Auth::user()->type == 'shopowner')
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop' || Route::currentRouteName() == 'shopowner.shop' || Route::currentRouteName() == 'shopowner.shop.edit') ? 'active' : '' }}" href="{{ route('shopowner.shop') }}">
-          {{ __('Details') }}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop.images' || Route::currentRouteName() == 'shopowner.shop.images' || Route::currentRouteName() == 'shopowner.shop.images.upload') ? 'active' : '' }}" href="{{ route('shopowner.shop.images') }}">
-          {{ __('Images') }}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop.services' || Route::currentRouteName() == 'shopowner.shop.services' || Route::currentRouteName() == 'shopowner.shop.services.add' || Route::currentRouteName() == 'shopowner.shop.services.edit') ? 'active' : '' }}" href="{{ route('shopowner.shop.services') }}">
-          {{ __('Services') }}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop.queue' || Route::currentRouteName() == 'shopowner.shop.queue') ? 'active' : '' }}" href="{{ route('shopowner.shop.queue') }}">
-          {{ __('Queue') }}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop.employees' || Route::currentRouteName() == 'shopowner.shop.employees' || Route::currentRouteName() == 'shopowner.shop.employees.add' || Route::currentRouteName() == 'shopowner.shop.employees.edit') ? 'active' : '' }}" href="{{ route('shopowner.shop.employees') }}">
-          {{ __('Employees') }}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop.reviews' || Route::currentRouteName() == 'shopowner.shop.reviews') ? 'active' : '' }}" href="{{ route('shopowner.shop.reviews') }}">
-          {{ __('Reviews') }}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link {{ (Route::currentRouteName() == 'shop.settings' || Route::currentRouteName() == 'shopowner.shop.settings') ? 'active' : '' }}" href="{{ route('shopowner.shop.settings') }}">
-          {{ __('Settings') }}
-        </a>
-      </li>
     @else
       <li class="nav-item">
         <a class="nav-link {{ (Route::currentRouteName() == 'shop' || Route::currentRouteName() == 'admin.shop') ? 'active' : '' }}" href="{{ route('shop', ['id' => $shop->id]) }}">
