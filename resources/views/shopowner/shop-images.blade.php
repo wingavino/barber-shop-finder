8fca928718b0a208c644dc4f31fad6a246063dc6
Erwin Gavino <erwingavino48@gmail.com>
2024-01-28 01:36:29 -0500

    Update shop-images.blade.php

--- resources/views/shopowner/shop-images.blade.php
+++ resources/views/shopowner/shop-images.blade.php
@@ -31,7 +31,7 @@
                     <div class="col-3">
                       <h4>Logo</h4>
                     @isset($logo)
-                        <a href="#" data-toggle="modal" data-target="#deleteModal" data-form-action="{{ route('shopowner.shop.images.delete', ['id' => $logo->id]) }}" data-id="{{ $logo->id }}" data-src="{{ asset('img/'.$logo->path) }}">
+                        <a href="#" data-toggle="modal" data-target="#deleteModal" data-form-action="{{ route('shopowner.shop.images.delete', ['id' => $shop->id, 'image_id' => $logo->id]) }}" data-id="{{ $logo->id }}" data-src="{{ asset('img/'.$logo->path) }}">
                           <img src="{{ asset('img/'.$logo->path) }}" class="img-thumbnail" alt="...">
                         </a>
                     @else
@@ -48,7 +48,7 @@
                     @isset($images)
                       @foreach($images as $image)
                         <div class="col-6">
-                          <a href="#" data-toggle="modal" data-target="#deleteModal" data-form-action="{{ route('shopowner.shop.images.delete', ['id' => $image->id]) }}" data-id="{{ $image->id }}" data-src="{{ asset('img/'.$image->path) }}">
+                          <a href="#" data-toggle="modal" data-target="#deleteModal" data-form-action="{{ route('shopowner.shop.images.delete', ['id' => $shop->id, 'image_id' => $image->id]) }}" data-id="{{ $image->id }}" data-src="{{ asset('img/'.$image->path) }}">
                             <img src="{{ asset('img/'.$image->path) }}" class="img-thumbnail" alt="...">
                           </a>
                         </div>
