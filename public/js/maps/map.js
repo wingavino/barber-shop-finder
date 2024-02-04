2993eea734dd820e78de1a4359cefdc619a326df
Erwin Gavino <erwingavino48@gmail.com>
2024-02-04 04:02:50 -0500

    Update map.js

--- public/js/maps/map.js
+++ public/js/maps/map.js
@@ -158,6 +158,29 @@ $(document).ready(function(){
   }
   getLocation(device);
 
+  $("#search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#shops-list a").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+  $('#collapseFilter').on('show.bs.collapse', function () {
+    $('#collapseFilterText').text("Filter -");
+  })
+
+  $('#collapseFilter').on('hide.bs.collapse', function () {
+    $('#collapseFilterText').text("Filter +");
+  })
+
+  $('#collapseShopList').on('show.bs.collapse', function () {
+    $('#collapseShopListText').text("Shops List -");
+  })
+
+  $('#collapseShopList').on('hide.bs.collapse', function () {
+    $('#collapseShopListText').text("Shops List +");
+  })
+
   $('#max_distance').on('input', function() {
     $('#max_distance_indicator').text(this.value);
     updateRadius(radiusCircle, device, this.value);
