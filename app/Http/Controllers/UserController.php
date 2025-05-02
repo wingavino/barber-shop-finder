d80af1714bfa9921af9057ffd4add8faefa54ad1
rebulan <79555303+rebulan@users.noreply.github.com>
2025-05-02 16:58:45 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -98,7 +98,7 @@ class UserController extends Controller
       return redirect()->route('admin.users');
     }
 
-    public function sendMobileOTP()
+    public function sendMobileOTP(NotificationController $notif)
     {
       /*$sms_active = env('SMS_ACTIVE');
 
@@ -130,7 +130,6 @@ class UserController extends Controller
       $isSuccessful = $sendCodeResponse->getSmsStatus() === "MESSAGE_SENT";
       $pinId = $sendCodeResponse->getPinId();*/
 	  $pinId = "122";
-	  $notif = new NotificationController;
 	  $notif->sms($mobile,$pinId);
 		//return redirect()->route('verify.mobile')->with(['message' => 'Code sent. Please check your phone.', 'pinId' => $pinId]);
 		return view('auth.verify-mobile', [
